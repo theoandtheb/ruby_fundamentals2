33989e097e98174516b4bb83effab283af54987f
theoandtheb <blue.apple@sympatico.ca>
2014-06-03 17:08:30 -0400

    A no-frills solution to exercise 6.

---
+++ exercise6.rb
@@ -0,0 +1,24 @@
+@grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def listed
+	@grocery_list.each do |item|
+	print "* #{item}\n"
+	end
+end
+
+listed
+@grocery_list << "rice"
+listed
+puts @grocery_list.count
+
+if @grocery_list.include?("bananas")
+	puts "You don't need to pick up bananas today"
+else
+	puts "You need to pick up bananas"
+end
+puts @grocery_list.at(1)
+@grocery_list = @grocery_list.sort
+listed
+g = @grocery_list.index("salmon")
+puts @grocery_list.at(g)
+@grocery_list.delete_at(g)
